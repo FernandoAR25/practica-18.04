0f933eae5e7e22a03741f2ea62665a2f2915762b
fernando <farenasr2511@gmail.com>
2024-04-22 22:16:50 -0600

    modificacion

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express =  require("express");
 const app= express();
 
-app. get ("/", (req,res)=>{
-    res. send("Hello world");
+app.use ((req,res,next)=>{
+    res.status (200);
+    next();
 });
 
-app. get ("/gordo", (req,res)=>{
+app. get ("/", (req,res)=>{
+    const usuario ={
+        metodo:"get",
+        nombre:"B",
+        role:"C",
+    };
+    res. json(usuario);
+});
+
+app. post("/gordo", (req,res)=>{
     res. send("hola haniel");
 });
+
+app. put("/gordo", (req,res)=>{
+    res. send("hola Fernando");
+});
+
 
 app. listen(8000, ()=> {
     console.log("Example app listening on port 8000!");
